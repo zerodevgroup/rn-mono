59ce562313a0952155c139d7fc8c4fea43e6745b
admin@zerodevgroup.com <admin@zerodevgroup.com>
2023-03-21 11:47:29 -0400

    mods

---
+++ shared/src/components/MyComponent/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+
+const MyComponent = () => (
+  <View>
+    MyComponent Component
+  </View>
+);
+
+MyComponent.propTypes = {}; 
+
+MyComponent.defaultProps = {}; 
+
+export default MyComponent;
